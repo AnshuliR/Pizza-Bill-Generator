097a3d798878456851ce866e6c9a1a8a06ae94e7
AnshuliR <anshuli1910@gmail.com>
2024-08-23 20:34:28 +0530

    Add Deluxe Pizza

---
+++ PizzaBillGenerator/src/pizzaBillGenerator/DeluxePizza.java
@@ -0,0 +1,21 @@
+package pizzaBillGenerator;
+
+public class DeluxePizza extends Pizza {
+
+	public DeluxePizza(boolean veg) {
+		super(veg);
+		super.addExtraCheese();
+		super.addExtraToppings();
+	}
+
+	@Override
+	public void addExtraCheese() {
+		
+	}
+
+	@Override
+	public void addExtraToppings() {
+		
+	}
+	
+}
